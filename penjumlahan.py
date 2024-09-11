1a99bfe80897f43480b268ca413e82ed46faea1c
wiranegara <wirajr297@gmail.com>
2024-09-11 11:40:59 +0800

    Create penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,13 @@
+# tugas-2# Program untuk menjumlahkan dua buah bilangan
+
+# Input bilangan pertama
+A = float(input("Masukkan bilangan pertama: "))
+
+# Input bilangan kedua
+B = float(input("Masukkan bilangan kedua: "))
+
+# Menjumlahkan kedua bilangan
+C = A + B
+
+# Menampilkan hasil penjumlahan
+print(f"Hasil dari {A} + {B} = {C}")
