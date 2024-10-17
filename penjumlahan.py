22716cfe2f6cd9a7ce6c1e8d1eb17447fba2a138
wiranegara <wirajr297@gmail.com>
2024-10-17 22:13:00 +0800

    Update penjumlahan.py

--- penjumlahan.py
+++ penjumlahan.py
@@ -1,13 +1,9 @@
-# tugas-2# Program untuk menjumlahkan dua buah bilangan
-
-# Input bilangan pertama
-A = float(input("Masukkan bilangan pertama: "))
-
-# Input bilangan kedua
-B = float(input("Masukkan bilangan kedua: "))
+ Meminta input dari pengguna
+bilangan1 = int(input("Masukkan bilangan pertama: "))
+bilangan2 = int(input("Masukkan bilangan kedua: "))
 
 # Menjumlahkan kedua bilangan
-C = A + B
+hasil = bilangan1 + bilangan2
 
-# Menampilkan hasil penjumlahan
-print(f"Hasil dari {A} + {B} = {C}")
+# Menampilkan hasil
+print("Hasil penjumlahan:", hasil)
