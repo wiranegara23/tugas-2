aaece820185a94cf6220dc2993a91a9fec65a344
wiranegara <wirajr297@gmail.com>
2024-10-17 22:15:17 +0800

    Update menghitung luas lingkaran.py

--- menghitung luas lingkaran.py
+++ menghitung luas lingkaran.py
@@ -1,10 +1,4 @@
-import math
-
-def hitung_luas_lingkaran(jari_jari):
-    luas = math.pi * (jari_jari ** 2)
-    return luas
-
-# Contoh penggunaan
-jari_jari = float(input("Masukkan jari-jari lingkaran: "))
-luas = hitung_luas_lingkaran(jari_jari)
-print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {luas:.2f}")
+ Menghitung luas lingkaran dengan pembulatan pi menjadi 3
+jari_jari = int(input("Masukkan jari-jari lingkaran: "))
+luas = 3 * jari_jari**2
+print(f"Luas lingkaran: {luas}")
